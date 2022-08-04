e60a0004fbd0c7b5b68bb5b2a960d283092f8b1d
jhoralek <horalek.jiri@gmail.com>
2022-08-04 12:24:27 +0000

    - version up

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="inels-mqtt",
-    version="0.0.24",
+    version="0.0.25",
     url="https://github.com/Nejezchleb/inels-mqtt",
     license="MIT",
     author="Elko EP s.r.o.",
